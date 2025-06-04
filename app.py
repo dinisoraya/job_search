aa6387fda5747b5053887394064e0c85e9acb877
dinisoraya <dinisoraya723@gmail.com>
2025-06-04 07:54:39 +0700

    import pandas

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import re
 import math
+import pandas as pd
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import (
